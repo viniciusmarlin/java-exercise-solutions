114274dee2357317e10d724fd277bac8a1886cee
Vinicius Santos <viniciusmarlinpessoal@gmail.com>
2026-07-13 13:24:03 -0300

    feat:  new exercises

---
+++ Solucoes/Switch/Ex01.java
@@ -0,0 +1,26 @@
+package Solucoes.Sequencias;
+
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        //Instancia do Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //Inputs
+
+        System.out.print("Escreva o primeiro número: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Escreva o segundo número: ");
+        int b = scanner.nextInt();
+
+        //Lógica
+        int calculo = a + b;
+
+        //Output
+        System.out.println("Soma: " + calculo);
+
+        scanner.close();
+    }
+}
